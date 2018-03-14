3df9a287dec728880d775d5cc0116e928517b5c5
Zak Ruvalcaba <zakruvalcaba@gmail.com>
2018-03-13 20:14:05 -0700

    Finished lab 11

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 /*eslint-env browser*/
 
+function init() {
+    "use strict";
+    var btnLogin = window.document.getElementById("login");
+    btnLogin.addEventListener("click", function () {
+        window.open("login.html", "login", "width=400,height=400");
+    });
+}
+window.addEventListener("load", init);
