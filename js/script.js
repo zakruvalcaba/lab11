f418eef5c780aab8b7e6bebda2a299a597721318
zakruvalcaba <zakruvalcaba@gmail.com>
2021-03-25 20:47:41 -0700

    updated JS to include ES6 loops

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
 // ADD EVENT HANDLER TO EACH H2 ELEMENT
 const toggle = (e) => {
-    // GET THE CURRENTLY SELECTED H2
+    // GET CURRENTLY SELECTED H2
     let h2 = e.currentTarget;
-    // GET THE H2 ELEMENT'S SIBLING DIV
+    // GET CURRENTLY SELECTED H2'S SIBLING DIV
     let div = h2.nextElementSibling;
-    // HANDLE THE REMOVAL OF CLASS ATTRIBUTES FROM H2 AND DIV TAGS
-    // GET H2 ELEMENTS AND THEN LOOP THROUGH THE ARRAY OF H2 ELEMENTS
+
+    // HANDLE REMOVAL OF ALL CLASSES FROM H2 AND DIV ELEMENTS
+    // LOOP THROUGH ARRAY OF H2 ELEMENTS
     let h2Elements = faqs.getElementsByTagName('h2');
-    for (let i = 0; i < h2Elements.length; i++) {
-        // IF THE H2 ELEMENT IN THE ARRAY DOESN'T EQUAL THE CURRENTLY
-        // CLICKED H2, HIDE ALL CLASS ATTRIBUTES FOR H2 AND CHILD DIVS
-        if (h2Elements[i] !== e.currentTarget) {
-            h2Elements[i].removeAttribute('class'); // REMOVE CLASS FROM H2
-            h2Elements[i].nextElementSibling.removeAttribute('class'); // REMOVE CLASS FROM DIV
+    for (h2Element of h2Elements) {
+        // IF THE H2 ELEMENT IN THE ARRAY DOESN't EQUAL THE CURRENTLY
+        // CLICKED H2, HIDE ALL CLASS ATTRIBUTES FOR H2 AND SIBLING DIVS
+        if (h2Element !== e.currentTarget) {
+            h2Element.removeAttribute('class'); // REMOVE CLASS FROM H2
+            h2Element.nextElementSibling.removeAttribute('class'); // REMOVE CLASS FROM SIBLING DIV
         }
     }
+
     // TOGGLE PLUS / MINUS ICON BY ADDING / REMOVING CLASS
     if (h2.hasAttribute('class')) {
         h2.removeAttribute('class');
     } else {
         h2.setAttribute('class', 'minus');
     }
+
     // TOGGLE DIV VISIBILITY BY ADDING / REMOVING CLASS
     if (div.hasAttribute('class')) {
         div.removeAttribute('class');
@@ -32,10 +35,16 @@ const toggle = (e) => {
 window.addEventListener('load', () => {
     // GET THE ACCORDION
     let faqs = document.querySelector('#faqs');
-    // GET THE H2 ELEMENTS FROM THE ACCORDION
+    // GET ALL THE H2 ELEMENTS FROM THE ACCORDION
     let h2Elements = faqs.getElementsByTagName('h2');
-    // ATTACH EVENT LISTENER AND HANDLER TO EACH H2 ELEMENT
-    for (let i = 0; i < h2Elements.length; i++) {
-        h2Elements[i].addEventListener('click', toggle);
+    // LOOP THROUGH COLLECTION OF H2 ELEMENTS AND ATTACH
+    // EVENT LISTENERS TO EACH H2
+    // USING ES5
+    // for (let i = 0; i < h2Elements.length; i++) {
+    //     h2Elements[i].addEventListener('click', toggle);
+    // }
+    // USING ES6
+    for (h2Element of h2Elements) {
+        h2Element.addEventListener('click', toggle);
     }
 });
